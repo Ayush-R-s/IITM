27d4b5a2c598103dc0ddfe292c5bc243d21ccbd2
Ayush <ayushthomas20@gmail.com>
2026-09-02 14:21:03 +0530

    new

---
+++ new.js
@@ -0,0 +1,30 @@
+function getNumbers() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    return { a, b };
+}
+
+function add() {
+    let { a, b } = getNumbers();
+
+    document.getElementById("result").innerHTML = "Result:"+(a + b);
+}
+
+function sub() {
+    let { a, b } = getNumbers();
+
+    document.getElementById("result").innerHTML = "Result:"+(a - b);
+}
+
+function mul() {
+    let { a, b } = getNumbers();
+
+    document.getElementById("result").innerHTML = "Result:"+(a * b);
+}
+
+function div() {
+    let { a, b } = getNumbers();
+
+    document.getElementById("result").innerHTML = "Result:"+(a / b);
+}
